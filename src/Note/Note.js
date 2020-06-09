297f7f7e583adf2a76dd2c65d5c7286bd13a7ce1
borisgrunwald <borisgrunwald@gmail.com>
2020-06-09 19:15:41 +0200

    Used ems instead of percent for note transform

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Note.css'
 
 export default ({x,y,val}) => 
-    <span className="note" style={{transform: `translate(${x*100}%,${y*100}%)`}}>{val}</span>
+    <span className="note" style={{transform: `translate(${x}em,${y}em)`}}>{val}</span>
 
 
 
